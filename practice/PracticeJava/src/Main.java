7be6a3652911261aa16632c8031c53be9557a3d3
nwnp-desktop <jjw_0418@naver.com>
2023-06-03 01:24:52 +0900

    test

---
+++ practice/PracticeJava/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, sum;
+        a = 1;
+        b = 2;
+        sum = a + b;
+        System.out.println(sum);
+    }
+}
